03da83a691e2873a2ce30394b4a1ce4a3ce60d1f
Manisha Sharma <manishafiji5194@gmail.com>
2016-04-24 23:08:17 -0700

    fixed flask templates to use bootstrap css files

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from flask import Flask, render_template,request, redirect, url_for
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 # we are able to make 2 different requests on our webpage
 # GET = we just type in the url
 # POST = some sort of form submission like a button
 
 stress_count = 0 # global stress count
 
-@app.route('/send_love', methods = ['POST'])
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/send_love', methods = ['POST', 'GET'])
 def send_love():
 
     # if we make a post request on the webpage aka press button then do stuff
 
-    if request.method == 'GET':
-        value = 10 #
+    if request.method == 'POST':
 
         # if send love button is pressed
-        if request.form['submit'] == 'Send Love':
-            print 'SEND_LOVE'
+        if request.form['submit'] == 'love':
+            print 'SENT LOVE!'
         else:
             pass
 
+        value = 10
+        # sent this as a post request to scarf
+
+
     # display happy.html page
-    return render_template('happy.html', value=value)
+    return render_template('happy.html')
 
 
 
@@ -43,4 +56,4 @@ if __name__ == "__main__":
     # lets launch our webpage!
     # do 0.0.0.0 so that we can log into this webpage
     # using another computer on the same network later
-    app.run(host='0.0.0.0')
+    app.run(debug=True)
