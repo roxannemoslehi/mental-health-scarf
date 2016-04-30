5b530ec9600b79d089299023c5b0eaed8ae8b025
Manisha Sharma <manishafiji5194@gmail.com>
2016-04-30 16:19:34 -0700

    added sound to wifi code! everything works yayaya

--- Arduino_code/WiFiWebClient/WiFiWebClient.ino
+++ Arduino_code/WiFiWebClient/WiFiWebClient.ino
@@ -13,6 +13,16 @@
  modified 31 May 2012
  by Tom Igoe
  */
+//A simple trigger for the Adafruit Audio FX Sound Board
+//For complete info on the sound board, see https://learn.adafruit.com/adafruit-audio-fx-sound-board/overview
+
+/*
+This is a simple test of a direct trigger of the Audio FX Sounds Board from an Arduino.
+For my test, I used an Arduino Pro Mini running at 3.3v & 8mHz. Digital pin #4 of the Arduino
+was connected to trigger pin #0 of the Sound Board, and I tied them to a common ground. I powered
+Sound Board via a USB external battery for the initial tests, and the Arduino via the FTDI cable.
+For later tests, I powered the Sound Board via the Arduino's VCC (3v) pin directly.
+*/
 
 
 #include <SPI.h>
@@ -20,6 +30,10 @@
 
 #include <Wire.h>
 #include "Adafruit_DRV2605.h"
+
+
+#define PIN 4
+#define SOUND_PIN 12
 
 
 
@@ -53,7 +67,7 @@ int keyIndex = 0;                // your network key Index number (needed only f
 
 Adafruit_DRV2605 drv;
 
-int pin1 = 6;
+int button = 6;
 volatile int state = LOW;
 
 long lastDebounceTime = 0;
@@ -88,12 +102,14 @@ void setup() {
 #ifdef WINC_EN
   pinMode(WINC_EN, OUTPUT);
   digitalWrite(WINC_EN, HIGH);
+  pinMode(button, INPUT_PULLUP);
+  setupSound(SOUND_PIN);
 #endif
 
 //  while (!Serial);  // required for Flora & Micro
   delay(500);
   Serial.begin(9600);
-  pinMode(pin1, INPUT_PULLUP);
+  pinMode(button, INPUT_PULLUP);
 
 // vibration testing
 //  Serial.println("DRV test");
@@ -220,12 +236,17 @@ void check_for_button_press() {
 //    if (client.connect(server, 80)) {
 
    // --------------------- BUTTON PRESSING  --------------------- 
-      if (digitalRead(pin1) == LOW && (millis() - lastDebounceTime) > debounceDelay) {
+      if (digitalRead(button) == LOW && (millis() - lastDebounceTime) > debounceDelay) {
             long pressed = millis();
             Serial.println("------ BUTTON PRESSED ------");
             post();
 //            pressed = true;
             lastDebounceTime = millis();
+
+            delay(500);
+            activateSound(SOUND_PIN);
+            delay(500);
+    
     //  } else {
     //        Serial.println("HIGH");
   
@@ -381,5 +402,23 @@ void vibrate() {
   drv.go();
 
   // wait a bit
+}
+
+void setupSound(int pin) {
+  Serial.println("Setting up Sound");
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, HIGH); // Set the pin high as the default state
+}
+
+void activateSound(int pin) {
+  Serial.println("Activating Sound");
+  digitalWrite(pin, LOW); // bring the pin low to begin the activation
+  /*
+  According to the documentation, the Audio FX board needs 50ms to trigger. However,
+  I've found that coming from my 3.3v Arduino Pro, it needs 100ms to get the trigger
+  the going
+  */
+  delay(500); // hold the pin low long enough to trigger the board; may need to be longer for consistent triggering
+  digitalWrite(pin, HIGH);// bring the pin high again to end the activation
 }
       
